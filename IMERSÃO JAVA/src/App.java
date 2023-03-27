ea03099f9b2505ce4afdbe653dd0f2d7c41d0975
raniel54 <ranielsilva65@hotmail.com>
2023-03-27 17:01:53 -0300

    update

--- IMERSÃO JAVA/src/App.java
+++ IMERSÃO JAVA/src/App.java
@@ -19,12 +19,18 @@ public class App {
         var request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
-        System.out.println(body);
 
         // buscar só os dados que interessam (titulo, poster, classificação)
         var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
-        // exibir e manipular os dados
 
+
+        // exibir e manipular os dados
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
     }
 }
